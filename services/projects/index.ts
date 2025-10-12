cffe9c4dbf8b66afb3e750e49c7afc5a516f05a4
Al-imraan Dawoood <alimraandawoodgulam@gmail.com>
2025-10-12 15:40:42 +0300

    Fixed issues concerning scrolling

--- services/projects/index.ts
+++ services/projects/index.ts
@@ -1,6 +1,7 @@
 import Pocketbase, { type RecordModel, type RecordSubscription } from 'pocketbase';
-const SERVER_URL = "http://192.168.5.1:8090";
-// const SERVER_URL = "https://www.practocore.com";
+
+const SERVER_URL = "https://www.practocore.com";
+// const SERVER_URL = "http://192.168.5.1:8090";
 
 const pocketbase = new Pocketbase(SERVER_URL);
 
