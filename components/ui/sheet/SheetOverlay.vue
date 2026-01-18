4cbb44eeccd14bc4350dbeef4a99c5d4f7200e6a
alimraanDawood <alimraandawoodgulam@gmail.com>
2026-01-18 18:28:39 +0300

    v1 alpha

--- components/ui/sheet/SheetOverlay.vue
+++ components/ui/sheet/SheetOverlay.vue
@@ -10,11 +10,25 @@ const delegatedProps = reactiveOmit(props, 'class')
 </script>
 
 <template>
-  <DialogOverlay
-    data-slot="sheet-overlay"
-    :class="cn('data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 fixed inset-0 z-50 bg-black/80', props.class)"
-    v-bind="delegatedProps"
-  >
-    <slot />
-  </DialogOverlay>
+  <Transition name="sheet-overlay" appear>
+    <DialogOverlay
+      data-slot="sheet-overlay"
+      :class="cn('fixed inset-0 z-50 bg-black/80 will-change-[opacity]', props.class)"
+      v-bind="delegatedProps"
+    >
+      <slot />
+    </DialogOverlay>
+  </Transition>
 </template>
+
+<style scoped>
+.sheet-overlay-enter-active,
+.sheet-overlay-leave-active {
+  transition: opacity 0.3s cubic-bezier(0.32, 0.72, 0, 1);
+}
+
+.sheet-overlay-enter-from,
+.sheet-overlay-leave-to {
+  opacity: 0;
+}
+</style>
